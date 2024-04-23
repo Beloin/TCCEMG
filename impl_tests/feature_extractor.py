d88a5d0e228d70d22e057d75cb3fc0158d4771be
Beloin Sena <beloin.rodrigues@gmail.com>
2024-04-22 21:18:44 -0300

    WIP - APPEN

--- impl_tests/feature_extractor.py
+++ impl_tests/feature_extractor.py
@@ -1,3 +1,4 @@
+import math
 from math import log
 from itertools import combinations
 import matplotlib.pyplot as plt
@@ -9,7 +10,7 @@ import pandas as pd
 # import nodls
 
 
-# |%%--%%| <KRXXGnMMAB|QcH1C2VbZC>
+#|%%--%%| <MhygeRZNUY|QcH1C2VbZC>
 
 fbase = pd.read_csv('./1_filtered.csv', header=None)
 
@@ -41,52 +42,29 @@ Sample entropy (SampEn) is a modification of approximate entropy (ApEn), used fo
 °°°"""
 # |%%--%%| <4pPo6KQ1QC|bSEp5543T3>
 
+import nolds
 
-def construct_templates(timeseries_data: list, m: int = 2):
-    num_windows = len(timeseries_data) - m + 1
-    return [timeseries_data[x: x + m] for x in range(0, num_windows)]
+nolds.sampen(cycle_01.grip[:int(.2*extractor._freq)])
 
+#|%%--%%| <bSEp5543T3|uRqC8wpvIV>
 
-def get_matches(templates: list, r: float):
-    return len(
-        list(filter(lambda x: is_match(
-            x[0], x[1], r), combinations(templates, 2)))
-    )
+data = []
+names = []
+for i in range(20):
+    window_s = .2 * (i+1)
+    window = window_s * extractor._freq
+    data.append(nolds.sampen((cycle_01.grip[:int(window)])))
+    names.append("{0:0.2f}s".format(window_s))
 
-
-def is_match(template_1: list, template_2: list, r: float):
-    return all([abs(x - y) < r for (x, y) in zip(template_1, template_2)])
-
-
-def sample_entropy(timeseries_data: list, window_size: int, r: float):
-    B = get_matches(construct_templates(timeseries_data, window_size), r)
-    A = get_matches(construct_templates(timeseries_data, window_size + 1), r)
-    return -log(A / B)
-
-# |%%--%%| <bSEp5543T3|oJlYWSM7Hs>
+data.append(nolds.sampen(cycle_01.grip[:int(window)]))
+names.append('{0:0.2f}s'.format(cycle_01.grip.__len__()/extractor._freq))
+print(data)
 
 
-fbase.head()
+plt.bar(names, data)
 
-# |%%--%%| <oJlYWSM7Hs|yIJHBh1f9x>
 
-# Cycles 4 Sensors with 5 Cycles
-cycle_01 = cycles[0][0]
-print(cycle_01)
-print(len(cycle_01.grip))
-cycle_01.grip.info()
-
-# |%%--%%| <yIJHBh1f9x|Fv3Fn2slGu>
-
-c_01std = np.std(cycle_01.grip)
-cycle_01.grip.head()
-
-# |%%--%%| <Fv3Fn2slGu|kh1XViUlqV>
-
-sample_entropy(cycle_01.grip, 0.5*extractor._freq,
-               c_01std)  # How to use withou integer?
-
-# |%%--%%| <kh1XViUlqV|RTbV6qOJTj>
+# |%%--%%| <uRqC8wpvIV|RTbV6qOJTj>
 r"""°°°
 # RMS - Root Mean Square 
 Is the sum of all squared values, divided by the amount and made a square root
@@ -107,16 +85,17 @@ cycle_01.grip.info()
 def rms(sample, window=200):
     return np.sqrt(np.mean(sample**2))
 
-#|%%--%%| <zHvvG2zHsf|pI4OKMOq8t>
+# |%%--%%| <zHvvG2zHsf|pI4OKMOq8t>
+
 
 rms(cycle_01.grip)
 
-#|%%--%%| <pI4OKMOq8t|ckJuZndQA1>
+# |%%--%%| <pI4OKMOq8t|ckJuZndQA1>
 
 # Choose the range, or the window
 rms(cycle_01.grip[:int(.2*extractor._freq)])
 
-#|%%--%%| <ckJuZndQA1|We2ZiuwrQg>
+# |%%--%%| <ckJuZndQA1|We2ZiuwrQg>
 
 data = []
 names = []
@@ -130,19 +109,19 @@ data.append(rms(cycle_01.grip[:int(window)]))
 names.append('{0:0.2f}s'.format(cycle_01.grip.__len__()/extractor._freq))
 print(data)
 
-    
+
 plt.bar(names, data)
 
 
-#|%%--%%| <We2ZiuwrQg|MtlNpD3ptX>
+# |%%--%%| <We2ZiuwrQg|MtlNpD3ptX>
 r"""°°°
 Waveform Length
 Waveform length is a measure of complexity of the EMG signal. It is defined as cumulative length of the EMG waveform over the time segment.
 
 ![Wave Formula](./wavelength_formula.png)
 °°°"""
-#|%%--%%| <MtlNpD3ptX|Sy0uAjQCNc>
-import math
+# |%%--%%| <MtlNpD3ptX|Sy0uAjQCNc>
+
 
 def waveformlen(sample):
     sum = 0
@@ -150,11 +129,12 @@ def waveformlen(sample):
         sum += math.fabs(sample[i+1] - sample[i])
     return sum
 
-#|%%--%%| <Sy0uAjQCNc|0tkSgscJkZ>
+# |%%--%%| <Sy0uAjQCNc|0tkSgscJkZ>
+
 
 waveformlen(np.array(cycle_01.grip[:int(.2*extractor._freq)]))
 
-#|%%--%%| <0tkSgscJkZ|YWWrqNXR2S>
+#|%%--%%| <0tkSgscJkZ|IftIPSWAmT>
 
 data = []
 names = []
@@ -167,7 +147,90 @@ for i in range(5):
 data.append(waveformlen(np.array(cycle_01.grip[:int(window)])))
 names.append('{0:0.2f}s'.format(cycle_01.grip.__len__()/extractor._freq))
 print(data)
-    
+
 plt.bar(names, data)
 
-# |%%--%%| <YWWrqNXR2S|GEDwmG2wVf>
+# |%%--%%| <IftIPSWAmT|INpH9OwTFZ>
+r"""°°°
+# Willison Amplitude
+Willison  amplitude  (WAMP)  is  the  number  of counts  for  each  change  in  the  EMG  signal amplitude  that  exceeds  a  predefine threshold
+
+![wamp](./wamp.png)
+°°°"""
+# |%%--%%| <INpH9OwTFZ|gXh8hBUUkY>
+
+
+def wamp(sample, threshold=10):
+    """
+    @param: sample - Sample based on volts
+    @param: threshold - Based on milivolts
+    """
+    threshold /= 1000
+    sum = 0
+    for i in range(len(sample)-1):
+        v = math.fabs(sample[i] - sample[i+1])
+        if v > threshold:
+            sum += 1
+    return sum
+
+
+#|%%--%%| <gXh8hBUUkY|k9YNHRMXNc>
+
+plt.plot(cycle_01.grip[:int(.2*extractor._freq)])
+
+#|%%--%%| <k9YNHRMXNc|zZNXRDd2WV>
+
+wamp(np.array(cycle_01.grip[:int(.2*extractor._freq)]))
+
+# |%%--%%| <zZNXRDd2WV|YWWrqNXR2S>
+
+data = []
+names = []
+for i in range(5):
+    window_s = .2 * (i+1)
+    window = window_s * extractor._freq
+    data.append(wamp(np.array(cycle_01.grip[:int(window)])))
+    names.append("{0:0.2f}s".format(window_s))
+
+data.append(wamp(np.array(cycle_01.grip[:int(window)])))
+names.append('{0:0.2f}s'.format(cycle_01.grip.__len__()/extractor._freq))
+print(data)
+
+plt.bar(names, data)
+
+#|%%--%%| <YWWrqNXR2S|PHUZqmqvOl>
+r"""°°°
+# Approximate Entropy
+ApEn, technique used to quantify the amount of regularity and the unpredictability of fluctuations over time-series data.
+
+The approximate entropy explains the complexity of the irregularity, quantifies how predictible the values of a time series are.
+°°°"""
+#|%%--%%| <PHUZqmqvOl|CwwddfAmvg>
+
+import EntropyHub as EH
+
+EH.ApEn(cycle_01.grip[:int(.2*extractor._freq)]) # Won't work, don't know why
+
+#|%%--%%| <CwwddfAmvg|0NP8vgSuEN>
+
+import antropy as an
+
+an.app_entropy(cycle_01.grip[:int(.2*extractor._freq)]) # Slow
+
+#|%%--%%| <0NP8vgSuEN|sL8IwFU5yR>
+
+data = []
+names = []
+for i in range(20):
+    window_s = .2 * (i+1)
+    window = window_s * extractor._freq
+    data.append(an.app_entropy(np.array(cycle_01.grip[:int(window)])))
+    names.append("{0:0.2f}s".format(window_s))
+
+data.append(an.app_entropy(np.array(cycle_01.grip[:int(window)])))
+names.append('{0:0.2f}s'.format(cycle_01.grip.__len__()/extractor._freq))
+print(data)
+
+plt.bar(names, data)
+
+# |%%--%%| <sL8IwFU5yR|GEDwmG2wVf>
